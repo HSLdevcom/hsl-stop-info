025ce3a3e092f2e431eff9d06c73b494958cdd80
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-11 12:54:40 +0300

    Upgrade to gatsby v2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'HSL Stop Info',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
@@ -11,9 +24,9 @@ module.exports = {
         exclude: /(node_modules|cache|public)/,
         options: {
           emitWarning: true,
-          failOnError: false,
-        },
-      },
-    },
+          failOnError: false
+        }
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div>
+  <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-  </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
 )
 
 export default IndexPage
